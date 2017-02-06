f4a6580bb44a5ad852b8f1895a0aa4fa640608ad
Patrik Cyvoct <patrik.cyvoct@gmail.com>
2017-02-06 21:50:12 +0100

    fix bug cd permission

--- src/functions/cd.rs
+++ src/functions/cd.rs
@@ -27,8 +27,11 @@ pub fn change_dir(mut args: String) {
             }
 
             if path.exists(){
-                env::set_current_dir(path)
-                    .expect("Failed to set current directory");
+                match env::set_current_dir(path) {
+                    Err(e) => println!("Error: {}", e),
+                    _ => (),
+                }
+                
             } else {
                 println!("{:?}", path);
                 println!("{} does not exist", args);
@@ -36,35 +39,5 @@ pub fn change_dir(mut args: String) {
 
         },
     }
-    //if args.split_whitespace().count() > 1 {
-    //    println!("Too many arguments for cd command");
-    //} else if args.split_whitespace().count() == 0 {
-    //    env::set_current_dir(Path::new(env::var("HOME")
-    //                                   .expect("no $HOME var in env").as_str()));
-    //} else {
-    //    let mut path = PathBuf::new();
-    //    let home_path = Path::new(env::var("HOME")
-    //                              .expect("no $HOME var in env").as_str());
-    //    if args.starts_with("~") {
-    //        path.push(home_path);
-    //        path.push(Path::new(args.split_off(1).as_str()));
-    //    } else {
-    //        path.push(Path::new(args.as_str()));
-    //    }
-    //
-    //    let mut path = path.as_path();
-    //
-    //    if path.is_relative() {
-    //        path = home_path.join(path).as_path();
-    //    }
-    //    if path.exists(){
-    //        env::set_current_dir(path)
-    //            .expect("Failed to set current directory");
-    //    } else {
-    //        println!("{} does not exist", args);
-    //    }
-    //
-    //}
-
 
 }
